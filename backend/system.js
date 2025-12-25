585665bf95ea59e0c196dc4d5f7280829abf11f5
executortion-wq <executortion@gmail.com>
2025-12-25 12:02:47 +0300

    Update system.js

--- backend/system.js
+++ backend/system.js
@@ -1,1 +1,21 @@
+const os = require("os");
+
+function getSystemStats() {
+  const totalMem = os.totalmem();
+  const freeMem = os.freemem();
+  const usedMem = totalMem - freeMem;
+
+  return {
+    cpuCores: os.cpus().length,
+    loadAvg: os.loadavg()[0].toFixed(2), // 1 dk load
+    memory: {
+      total: (totalMem / 1024 / 1024 / 1024).toFixed(2),
+      used: (usedMem / 1024 / 1024 / 1024).toFixed(2),
+      free: (freeMem / 1024 / 1024 / 1024).toFixed(2)
+    },
+    uptime: os.uptime()
+  };
+}
+
+module.exports = { getSystemStats };
 
